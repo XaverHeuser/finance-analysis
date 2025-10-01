7fdeacd393c72f8194d0bcb1441667e2bf8bf03e
XaverHeuser <xaver.heuser@gmail.com>
2025-10-01 16:42:02 +0200

    enhancement

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 """This file processes an account statement and writes the data into a GSheet."""
 
+import logging
 import os
 import sys
 
@@ -29,8 +30,11 @@ from utils.google_services import (
 client, service = set_up_google_connection()
 
 # load_dotenv()
+SPREADSHEET_ID = os.environ.get('SPREADSHEET_ID')
 TEMP_FOLDER_ID = os.environ.get('TEMP_FOLDER_ID')
 REGULAR_FOLDER_ID = os.environ.get('REGULAR_FOLDER_ID')
+
+# TODO: Implement check if IDs are not None
 
 
 #############
@@ -39,100 +43,79 @@ REGULAR_FOLDER_ID = os.environ.get('REGULAR_FOLDER_ID')
 def process_account_statements() -> None:
     """Process all account statements in the downloads folder."""
     # Get all account statement files from GDrive temp folder
-    acc_files = get_acc_files_from_gdrive_folder(TEMP_FOLDER_ID, service)
+    acc_states = get_acc_files_from_gdrive_folder(TEMP_FOLDER_ID, service)
 
     # Process each account statement file
-    for acc_file in acc_files:
+    for acc_state in acc_states:
         # Check if file matches criteria
-        if 'Kontoauszug' not in acc_file['name']:
-            print(f'Skipping file {acc_file["name"]} as it does not match criteria.')
+        if 'Kontoauszug' not in acc_state['name']:
+            logging.warning(
+                f'Skipping file {acc_state["name"]} as it does not match criteria.'
+            )
             continue
 
         # Standardize file name
-        new_name = '_'.join(acc_file['name'].split('_')[:4])
+        new_name = '_'.join(acc_state['name'].split('_')[:4])
         if not new_name.lower().endswith('.pdf'):
             new_name += '.pdf'
-        acc_file['name'] = new_name
+        acc_state['name'] = new_name
 
         # Check if file already processed
-        if check_if_file_in_folder(acc_file['name'], service, REGULAR_FOLDER_ID):
-            print(f'File {acc_file["name"]} already processed. Skipping.')
+        if check_if_file_in_folder(acc_state['name'], service, REGULAR_FOLDER_ID):
+            logging.info(f'File {acc_state["name"]} already processed. Skipping.')
             continue
 
         # Get spreadsheet ID based on year in file name
-        acc_file_year = acc_file['name'].split('_')[1]
-        gsheet_file = f'SPREADSHEET_ID_{acc_file_year}'
-        spreadsheet_id = os.environ.get(gsheet_file)
-        if spreadsheet_id is None:
-            print(f'No spreadsheet ID found for year {acc_file_year}. Skipping file.')
-            continue
-        print(f'Processing file {acc_file["name"]} into spreadsheet {spreadsheet_id}.')
+        acc_state_year = acc_state['name'].split('_')[1]
 
         # Extract text from PDF
-        full_pdf_text = extract_text_from_pdf(acc_file['id'], service)
+        full_pdf_text = extract_text_from_pdf(acc_state['id'], service)
         lines = full_pdf_text.split('\n')
-        print(f'Extracted {len(lines)} lines from PDF.')
+        logging.info(f'Extracted {len(lines)} lines from PDF.')
 
         # Get account balances and transactions
         acc_balance_old = get_balance_of_account(lines, 'alter Kontostand')
         acc_balance_new = get_balance_of_account(lines, 'neuer Kontostand')
-        print(
+        logging.info(
             f'Old acc value, Value: {acc_balance_old[0]}€ - Line-Index: {acc_balance_old[1]}'
         )
-        print(
+        logging.info(
             f'New acc value, Value: {acc_balance_new[0]}€ - Line-Index: {acc_balance_new[1]}'
         )
         all_transactions = get_all_transactions(
             lines, acc_balance_old[1], acc_balance_new[1]
         )
-        print(f'Count of transactions: {len(all_transactions)}')
+        logging.info(f'Count of transactions: {len(all_transactions)}')
 
         # Open Google Sheets
-        spreadsheet = client.open_by_key(spreadsheet_id)
+        spreadsheet = client.open_by_key(SPREADSHEET_ID)
+        sheet_transactions = spreadsheet.worksheet('Transaktionen')
 
-        sheet_incomes = spreadsheet.worksheet('Einnahmen')
-        sheet_expenses = spreadsheet.worksheet('Ausgaben')
-
-        # Load data from sheets into dataframes
-        df_expenses = pd.DataFrame(sheet_expenses.get_all_values())
-        df_incomes = pd.DataFrame(sheet_incomes.get_all_values())
-
-        df_expenses.columns = df_expenses.iloc[0]
-        df_expenses = df_expenses[1:].reset_index(drop=True)
-
-        df_incomes.columns = df_incomes.iloc[0]
-        df_incomes = df_incomes[1:].reset_index(drop=True)
-
-        gsheets = {'Expense': df_expenses, 'Income': df_incomes}
+        # Load existing data from Google Sheets into DataFrames
+        df_sheet_transactions = pd.DataFrame(sheet_transactions.get_all_values())
+        df_sheet_transactions.columns = df_sheet_transactions.iloc[0]
+        df_sheet_transactions = df_sheet_transactions[1:].reset_index(drop=True)
 
         # Process each transaction
-        for transaction_index in range(len(all_transactions)):
-            transaction = all_transactions[transaction_index]
+        for t_index in range(len(all_transactions)):
+            transaction = all_transactions[t_index]
 
             # Extract transaction info
-            transaction_type, df, name, transaction_value, month = (
-                extract_transaction_info(transaction, gsheets)
+            t_name, t_value, t_type, t_date = extract_transaction_info(
+                transaction=transaction, transaction_year=acc_state_year
             )
 
             # Add new row to dataframe
-            add_new_row(
-                df,
-                name,
-                month,
-                transaction_value,
-                transaction_type,
-                gsheets,
-                general_account=True,
-            )
+            add_new_row(df_sheet_transactions, t_name, t_value, t_type, t_date)
 
         # Update Google Sheets with new data
-        update_google_sheet(sheet_expenses, gsheets['Expense'])
-        update_google_sheet(sheet_incomes, gsheets['Income'])
-        print('All changes saved to Google Sheets!')
+        update_google_sheet(sheet_transactions, df_sheet_transactions)
+        logging.info('All changes saved to Google Sheets!')
 
         # Move processed file to regular folder
-        file_id = acc_file['id']
-        move_file(file_id, acc_file['name'], TEMP_FOLDER_ID, REGULAR_FOLDER_ID, service)
+        file_id = acc_state['id']
+        file_name = acc_state['name']
+        move_file(file_id, file_name, TEMP_FOLDER_ID, REGULAR_FOLDER_ID, service)
 
 
 if __name__ == '__main__':
